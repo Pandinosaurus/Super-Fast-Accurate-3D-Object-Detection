a70203a34236f94457be85879237130a9d6ac6b1
Nguyen Mau Dung <nguyenmaudung93.kstn@gmail.com>
2021-01-02 16:36:07 +0900

    remove redundant code

--- sfa/data_process/kitti_bev_utils.py
+++ sfa/data_process/kitti_bev_utils.py
@@ -20,13 +20,10 @@ import config.kitti_config as cnf
 
 
 def makeBEVMap(PointCloud_, boundary):
-    Height = cnf.BEV_HEIGHT + 1
-    Width = cnf.BEV_WIDTH + 1
-
     # Discretize Feature Map
     PointCloud = np.copy(PointCloud_)
     PointCloud[:, 0] = np.int_(np.floor(PointCloud[:, 0] / cnf.DISCRETIZATION))
-    PointCloud[:, 1] = np.int_(np.floor(PointCloud[:, 1] / cnf.DISCRETIZATION) + Width / 2)
+    PointCloud[:, 1] = np.int_(np.floor(PointCloud[:, 1] / cnf.DISCRETIZATION) + cnf.BEV_WIDTH / 2)
 
     # sort-3times
     sorted_indices = np.lexsort((-PointCloud[:, 2], PointCloud[:, 1], PointCloud[:, 0]))
@@ -34,25 +31,18 @@ def makeBEVMap(PointCloud_, boundary):
     _, unique_indices, unique_counts = np.unique(PointCloud[:, 0:2], axis=0, return_index=True, return_counts=True)
     PointCloud_top = PointCloud[unique_indices]
 
-    # Height Map, Intensity Map & Density Map
-    heightMap = np.zeros((Height, Width))
-    intensityMap = np.zeros((Height, Width))
-    densityMap = np.zeros((Height, Width))
+    # Intensity Map, Height Map & Density Map (IHD)
+    IHD_Map = np.zeros((3, cnf.BEV_HEIGHT, cnf.BEV_WIDTH))
 
-    # some important problem is image coordinate is (y,x), not (x,y)
+    IHD_Map[0, np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
+
     max_height = float(np.abs(boundary['maxZ'] - boundary['minZ']))
-    heightMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 2] / max_height
+    IHD_Map[1, np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 2] / max_height
 
     normalizedCounts = np.minimum(1.0, np.log(unique_counts + 1) / np.log(64))
-    intensityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = PointCloud_top[:, 3]
-    densityMap[np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = normalizedCounts
+    IHD_Map[2, np.int_(PointCloud_top[:, 0]), np.int_(PointCloud_top[:, 1])] = normalizedCounts
 
-    RGB_Map = np.zeros((3, Height - 1, Width - 1))
-    RGB_Map[2, :, :] = densityMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # r_map
-    RGB_Map[1, :, :] = heightMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # g_map
-    RGB_Map[0, :, :] = intensityMap[:cnf.BEV_HEIGHT, :cnf.BEV_WIDTH]  # b_map
-
-    return RGB_Map
+    return IHD_Map
 
 
 # bev image coordinates format
